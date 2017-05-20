a0e529ceadd911e4c49f4dc3d36b800ac7fdc3d9
William Fleming <will.fleming12@btinternet.com>
2017-05-20 01:15:29 +0100

    add basic layout of AddDataPage

--- src/components/AddDataPage.js
+++ src/components/AddDataPage.js
@@ -2,8 +2,40 @@ import React from 'react';
 
 const AddDataPage = () => {
   return (
-    <div>
-      <h1>ADD DATA</h1>
+    <div className='section'>
+      <div className='container'>
+
+        <div className='field'>
+          <label className='label'>xml Source Directory:</label>
+          <p className='control'>
+            <input 
+                className='input'
+                type='text' 
+                placeholder='path to directory'
+            />
+          </p>
+        </div>
+
+        <div className='field'>
+          <label className='label'>output directory:</label>
+          <p className='control'>
+            <input 
+                className='input'
+                type='text' 
+                placeholder='path to directory'
+            />
+          </p>
+        </div>
+
+        <div className='section centre-content'>
+          <div className='field'>
+            <p className='control'>
+              <button className='button is-primary is-large'>Parse!</button>
+            </p>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 };
