74d99f990b9a70f13a039b51756fed859549a9a4
William Fleming <will.fleming12@btinternet.com>
2017-05-20 11:44:08 +0100

    add functionality to input boxes

--- src/components/AddDataPage.js
+++ src/components/AddDataPage.js
@@ -1,43 +1,71 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const AddDataPage = () => {
-  return (
-    <div className='section'>
-      <div className='container'>
+class AddDataPage extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      inputDir: '',
+      outputDir: ''
+    };
+    this.updateInputDir = this.updateInputDir.bind(this);
+    this.updateOutputDir = this.updateOutputDir.bind(this);
+  }
 
-        <div className='field'>
-          <label className='label'>xml Source Directory:</label>
-          <p className='control'>
-            <input 
-                className='input'
-                type='text' 
-                placeholder='path to directory'
-            />
-          </p>
-        </div>
+  updateInputDir (event) {
+    this.setState({
+      inputDir: event.target.value
+    });
+  }
 
-        <div className='field'>
-          <label className='label'>output directory:</label>
-          <p className='control'>
-            <input 
-                className='input'
-                type='text' 
-                placeholder='path to directory'
-            />
-          </p>
-        </div>
+  updateOutputDir (event) {
+    this.setState({
+      outputDir: event.target.value
+    });
+  }
 
-        <div className='section centre-content'>
+  render () {
+    return (
+      <div className='section'>
+        <div className='container'>
+
           <div className='field'>
+            <label className='label'>xml Source Directory:</label>
             <p className='control'>
-              <button className='button is-primary is-large'>Parse!</button>
+              <input 
+                  className='input'
+                  type='text' 
+                  placeholder='path to directory'
+                  value={this.state.inputDir}
+                  onChange={this.updateInputDir}
+              />
             </p>
           </div>
-        </div>
 
+          <div className='field'>
+            <label className='label'>output directory:</label>
+            <p className='control'>
+              <input 
+                  className='input'
+                  type='text' 
+                  placeholder='path to directory'
+                  value={this.state.outputDir}
+                  onChange={this.updateOutputDir}
+              />
+            </p>
+          </div>
+
+          <div className='section centre-content'>
+            <div className='field'>
+              <p className='control'>
+                <button className='button is-primary is-large'>Parse!</button>
+              </p>
+            </div>
+          </div>
+
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default AddDataPage;
